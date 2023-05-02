8edc19fb6ce3521a50c01cd051ade04e0a0f566b
LegoGAME <i.hate.radeon@gmail.com>
2023-05-03 00:32:12 +0300

    add surround

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,17 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+  -- nvim-surround
+  -- https://github.com/kylechui/nvim-surround
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 }
